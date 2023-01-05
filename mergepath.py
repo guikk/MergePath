76a28e80f8cbd883937d7b385e88ef33f3d7f88c
Guilherme Klein Kern <guilherme.klein-kern@grenoble-inp.org>
2023-01-05 13:05:30 +0100

    Implement paper pseudocode

--- mergepath.py
+++ mergepath.py
@@ -1,47 +1,66 @@
-def parallelMerge(arr, l, m, r):
-    n1 = m - l + 1
-    n2 = r - m
- 
-    # create temp arrays
-    L = [0] * (n1)
-    R = [0] * (n2)
- 
-    # TODO: implement the Merge Path algorithm
+DEFAULT_THREADS = 4
 
-    # Copy data to temp arrays L[] and R[]
-    for i in range(0, n1):
-        L[i] = arr[l + i]
- 
-    for j in range(0, n2):
-        R[j] = arr[m + 1 + j]
- 
-    # Merge the temp arrays back into arr[l..r]
-    i = 0     # Initial index of first subarray
-    j = 0     # Initial index of second subarray
-    k = l     # Initial index of merged subarray
- 
-    while i < n1 and j < n2:
-        if L[i] <= R[j]:
-            arr[k] = L[i]
-            i += 1
-        else:
-            arr[k] = R[j]
-            j += 1
-        k += 1
- 
-    # Copy the remaining elements of L[], if there
-    # are any
-    while i < n1:
-        arr[k] = L[i]
-        i += 1
-        k += 1
- 
-    # Copy the remaining elements of R[], if there
-    # are any
-    while j < n2:
-        arr[k] = R[j]
-        j += 1
-        k += 1
- 
-# l is for left index and r is right index of the
-# sub-array of arr to be sorted
+class ParallelMerger:
+    """
+    In-place parallel merger of two adjacent segments of one array
+    """
+    def __init__(self, threads, array, left, mid, right):
+        self.p = threads
+        self.A = array[left:mid+1]
+        self.B = array[mid+1:right+1]
+        self.S = array
+        self.segmentLength = (len(self.A) + len(self.B)) // self.p
+
+    def compute(self):
+        for i in range(self.p):
+            si = i * self.segmentLength + 1
+            ai, bi = self.findIntersection(i)
+            self.merge(ai, bi, si, self.segmentLength)
+
+
+    def merge(self, ai, bi, si, length):
+        pass
+
+    def M(self, i, j):
+        """
+        Compute merge matrix element
+        • i and j are 1-indexed
+        """
+
+        if i == 0 or j > len(self.B):
+            return False # out of bounds up right
+
+        if j == 0 or i > len(self.A):
+            return True # out of bounds down left
+
+        return self.A[i-1] > self.B[j-1]
+
+    def findIntersection(self, diagonalIndex):
+        """
+        Compute point where the merge path crosses the ith diagonal
+        """
+
+        if diagonalIndex == 0:
+            return (0, 0)
+
+        diag = (diagonalIndex + 1) * self.segmentLength
+        atop = len(self.A) if diag > len(self.A) else diag
+        btop = diag - len(self.A) if diag > len(self.A) else 0
+        abottom = btop
+
+        while True:
+            offset = (atop - abottom) // 2
+            ai = atop - offset
+            bi = btop + offset
+            if self.M(ai,bi-1):
+                if not self.M(ai-1,bi):
+                    return (ai, bi)
+                else:
+                    atop = ai - 1
+                    btop = bi + 1
+            else:
+                abottom = ai + 1
+
+def parallelMerge(arr, l, m, r):
+    pm = ParallelMerger(DEFAULT_THREADS, arr, l, m, r)
+    pm.compute()
